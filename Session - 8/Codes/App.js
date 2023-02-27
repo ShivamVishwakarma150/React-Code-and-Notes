2ae4a6f4710828137885fe6c630c3afca694e707
Shivam Vishwakarma <arvind.972176@gmail.com>
2023-02-27 20:46:07 +0530

    27-02-2023

---
+++ Session - 8/Codes/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { NetflixRegisterComponent } from "./src/components/NetflixRegisterComponent";
+import './node_modules/bootstrap/dist/css/bootstrap.css';
+import './node_modules/bootstrap-icons/font/bootstrap-icons.css';
+
+import NetflixIndexComponent from "./src/components/NetflixIndexComponent";
+import RegisterComponent from "./src/components/RegisterComponent";
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Passing a react element inside the root
+root.render(
+    <React.StrictMode>
+       <RegisterComponent/>
+    </React.StrictMode>
+);
---
+++ Session - 8/Codes/src/components/NetflixFooterComponent.js
@@ -0,0 +1,38 @@
+export default function NetflixFooterComponent()
+{
+    return(
+        <>
+            Questions? Call 000-800-919-1694
+            <div className="row">
+                <div className="col">
+                        <ul className="list-unstyled">
+                            <li>FAQHelp</li>
+                            <li>Investor Relations</li>
+                            <li>Privacy</li>
+                        </ul>
+                </div>
+                <div className="col">
+                        <ul className="list-unstyled">
+                            <li>Help Centre</li>
+                            <li>Jobs</li>
+                            <li>Cookie Preferences</li>
+                        </ul>
+                </div>
+                <div className="col">
+                         <ul className="list-unstyled">
+                            <li>Help Centre</li>
+                            <li>Jobs</li>
+                            <li>Cookie Preferences</li>
+                        </ul>
+                </div>
+                <div className="col">
+                     <ul className="list-unstyled">
+                            <li>Help Centre</li>
+                            <li>Jobs</li>
+                            <li>Cookie Preferences</li>
+                        </ul>
+                </div>
+            </div>
+        </>
+    )
+};
